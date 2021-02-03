b7ffcb2533fa68bbcb8437dfccc780f27cc6f054
Vinicius Lordelo <73176059+ViniciusLordelo@users.noreply.github.com>
2021-02-03 10:43:52 -0300

    Page Finalizada

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -20,7 +20,7 @@ namespace InstaDev_s.Controllers
 
         
 
-        [Route ("Cadastrar_Usuario")]
+        [Route ("CadastrarUsuario")]
         public IActionResult CadastrarUsuario (IFormCollection form)
         {
             Usuario novoUsuario = new Usuario();
@@ -30,7 +30,7 @@ namespace InstaDev_s.Controllers
             novoUsuario.Username = form["Username"];
             novoUsuario.Email = form["Email"];
             novoUsuario.Senha = form["Senha"];
-            // novoUsuario.DataNascimento = DateTime.Parse(form["Data de Nascimento"]);
+            novoUsuario.DataNascimento = DateTime.Parse(form["DataNascimento"]);
             novoUsuario.Foto = form["Foto"];
             
             if (form.Files.Count > 0)
@@ -59,7 +59,7 @@ namespace InstaDev_s.Controllers
             if(novoUsuario.Nome != null && novoUsuario.Email != null && novoUsuario.Senha != null && novoUsuario.Username != null)
             {
                 usuarioModel.CadastrarUsuario(novoUsuario);
-                ViewBag.Usuario = usuarioModel.Cadastro();
+                // ViewBag.Usuario = usuarioModel.Cadastro();
             }else{
                 usuarioModel.Mensagem = "Preencha todos os campos!";
             }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -42,25 +42,25 @@ namespace InstaDev_s.Models
             string[] linhas = {Prepare(u)};
             File.AppendAllLines(PATH, linhas);
         }
-        public List<Usuario> Cadastro(){ //Perfil
-            List<Usuario> usuarios = new List<Usuario>();
-            string[] linhas = File.ReadAllLines(PATH);
-            foreach (var item in linhas)
-            {
-                string[] linha = item.Split(";");
-                Usuario novoUsuario = new Usuario();
-                novoUsuario.IdUsuario = Guid.Parse(linha[0]);
-                novoUsuario.Foto = linha[1];
-                novoUsuario.DataNascimento = DateTime.Parse(linha[2]);
-                novoUsuario.Nome = linha[3];
-                novoUsuario.Email = linha[4];
-                novoUsuario.Username = linha[5];
-                novoUsuario.Senha = linha[6];
-                // novoUsuario.Seguidos = Int32[].Parse(linha[7]);
+        // public List<Usuario> Cadastro(){ //Perfil
+        //     List<Usuario> usuarios = new List<Usuario>();
+        //     string[] linhas = File.ReadAllLines(PATH);
+        //     foreach (var item in linhas)
+        //     {
+        //         string[] linha = item.Split(";");
+        //         Usuario novoUsuario = new Usuario();
+        //         novoUsuario.IdUsuario = Guid.Parse(linha[0]);
+        //         novoUsuario.Foto = linha[1];
+        //         novoUsuario.DataNascimento = DateTime.Parse(linha[2]);
+        //         novoUsuario.Nome = linha[3];
+        //         novoUsuario.Email = linha[4];
+        //         novoUsuario.Username = linha[5];
+        //         novoUsuario.Senha = linha[6];
+        //         // novoUsuario.Seguidos = Int32[].Parse(linha[7]);
 
-            }
-            return usuarios;
-        }   
+        //     }
+        //     return usuarios;
+        // }   
 
         public void EditarUsuario(Usuario u){
             List<string> linhas  = ReadAllLinesCSV(PATH);
